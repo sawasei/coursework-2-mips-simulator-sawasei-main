cfe3fbeb7d1b10c796caff0a03cf1fca50557d57
Zezheng Liu <aichisawasei@gmail.com>
2024-11-25 02:01:20 +0000

    s2576948

--- src/global.h
+++ src/global.h
@@ -8,3 +8,4 @@ extern uint32_t registers[32];         // MIPS register file (32 registers)
 extern uint32_t hi, lo;                // HI and LO registers for multiplication/division
 
 #endif // GLOBAL_H_
+1
